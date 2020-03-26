df7a33d3eaeea05afe0f9a1bdb50cd5c521f412c
Ramu Sunkari <ramusunkari444@gmail.com>
2020-03-26 22:33:40 +0530

    welcome message added

--- src/main/java/com/springsecurity/springbootsecuritydemo/SpringbootsecuritydemoApplication.java
+++ src/main/java/com/springsecurity/springbootsecuritydemo/SpringbootsecuritydemoApplication.java
@@ -2,12 +2,20 @@ package com.springsecurity.springbootsecuritydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootsecuritydemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootsecuritydemoApplication.class, args);
 	}
 
+	@RequestMapping(value="/welcome",method=RequestMethod.GET)
+	public String welcome() {
+		return "welcome to GitHub";
+	}
 }
